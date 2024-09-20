8fff6e67c07aefa138d6cab1a2f32dae932a8d2d
officialkee <shikee.maloney@outlook.com>
2024-09-20 14:42:34 -0400

    applied foundational logic

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main 
 
+import(
+	"encoding/json"
+	"fmt"
+)
+
 func main(){
+
+	 fake := `{"key": "value"}`
+
+	jsonMap := make(map[string]interface{})
+
+	err := json.Unmarshal([]byte(fake),&jsonMap)
+
+	if err != nil {
+		fmt.Println("Err: ",err)
+		return
+	}
 	
+	fmt.Println(jsonMap["key"])
+	
+
 }
