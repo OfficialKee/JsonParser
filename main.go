a49824b48ea6c6e4a9117d0e209c8fef857384e2
officialkee <shikee.maloney@outlook.com>
2024-09-20 15:21:22 -0400

    added input from commandline function

--- main.go
+++ main.go
@@ -3,22 +3,35 @@ package main
 import(
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 func main(){
 
-	 fake := `{"key": "value"}`
+	args := os.Args
+	// fake := `{"key": "value"}`
+	jsonString := args[1]
+	if len(args) > 1 {
+		parseJson(jsonString)
+		// fmt.Println(jsonString)
+	}
 
+	
+	
+
+}
+
+func parseJson(jsonString string) int {
+	
 	jsonMap := make(map[string]interface{})
 
-	err := json.Unmarshal([]byte(fake),&jsonMap)
+	err :=  json.Unmarshal([]byte(jsonString),&jsonMap)
 
 	if err != nil {
 		fmt.Println("Err: ",err)
-		return
+		return 1
 	}
 	
-	fmt.Println(jsonMap["key"])
-	
-
+	fmt.Println(jsonMap)
+	return 0
 }
